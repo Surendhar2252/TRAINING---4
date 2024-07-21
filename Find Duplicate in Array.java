0fc4e7de344ed4c1fe7bbd684c362528d2fa7e2e
Surendhar M S <139234490+Surendhar2252@users.noreply.github.com>
2024-07-21 10:03:28 +0530

    Create Find Duplicate in Array.java

---
+++ Find Duplicate in Array.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return num;
+            set.add(num);
+        }
+        return -1; // No duplicate found
+    }
+}
